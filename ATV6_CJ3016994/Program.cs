aa20502d41a672d0617dfe49d433691cd0503135
CJ3016994 <CJ3016994@LAB03-PC18>
2024-04-10 12:12:40 -0300

    Adicionar arquivos de projeto.

---
+++ ATV6_CJ3016994/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATV6_CJ3016994
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ex1
+            //Console.WriteLine("Digite um número inteiro:");
+            //int numero = int.Parse(Console.ReadLine());
+
+            //int contador = 1; // Inicializa o contador com 1
+            //{
+            //    Console.WriteLine(contador);
+            //    contador++; 
+            //    Console.ReadKey();
+
+            //}
+            //    ex2
+            //    Console.WriteLine("Digite um número inteiro:");
+            //    int numero = int.Parse(Console.ReadLine());
+
+            //    int contador = 1;
+            //    Console.WriteLine("Números pares de 1 até " + numero + ":");
+
+            //    while (contador <= numero)
+            //    {
+            //        if (contador % 2 == 0)
+            //        {
+            //            Console.WriteLine(contador);
+            //        }
+            //        contador++; 
+            //        Console.ReadKey();
+            //    }
+
+            //ex3
+            //int numero;
+
+            //do
+            //{
+            //    Console.WriteLine("Digite um número inteiro menor que 1000:");
+            //    numero = int.Parse(Console.ReadLine());
+            //} while (numero >= 1000); 
+
+            //int contador = 1000; 
+
+            //Console.WriteLine("Números pares de 1000 até " + numero + ":");
+
+            //while (contador <= numero)
+            //{
+            //    if (contador % 2 == 0) 
+            //    {
+            //        Console.WriteLine(contador);
+            //    }
+            //    contador++; 
+            //    Console.ReadKey();
+            //}
+            //ex4
+            //int soma = 0;
+
+            //Console.WriteLine("Digite uma sequência de números inteiros (digite um número negativo para encerrar):");
+
+            //while (soma < 200)
+            //{
+            //    int numero = int.Parse(Console.ReadLine());
+
+            //    if (numero < 0) 
+            //    {
+            //        break;
+            //    }
+            //    else if (numero > 0) 
+            //    {
+            //        soma += numero; 
+            //        Console.WriteLine(numero)
+
+            //Console.WriteLine("Soma dos números positivos: " + soma);
+            //Console.ReadKey();
+
+            //ex5
+            //Console.WriteLine("Digite um número inteiro:");
+            //int numero = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Divisores de " + numero + ":");
+
+            //// Loop para encontrar e imprimir os divisores
+            //for (int divisor = 1; divisor <= numero; divisor++)
+            //{
+            //    if (numero % divisor == 0) 
+            //    {
+            //        Console.WriteLine(divisor);
+            //        Console.ReadKey();
+            //    }
+            }
+       }
+    }
+//ex6
+//int contador = 1;
+//int maior = int.MinValue; 
+//int menor = int.MaxValue; // Inicializa com o maior valor possível
+
+//while (contador <= 10)
+//{
+//    Console.WriteLine("Digite o " + contador + "º número:");
+//    int numero = int.Parse(Console.ReadLine());
+
+//    if (numero > maior)
+//    {
+//        maior = numero;
+//    }
+
+//    if (numero < menor)
+//    {
+//        menor = numero;
+//    }
+
+//    contador++;
+//}
+
+//Console.WriteLine("O maior número digitado foi: " + maior);
+//Console.WriteLine("O menor número digitado foi: " + menor);
+//Console.ReadKey();
+
+//ex7
+//int somaPares = 0;
+//int somaImpares = 0;
+
+//Console.WriteLine("Digite uma sequência de números (digite 0 para encerrar):");
+
+//int numero;
+//while (true)
+//{
+//    Console.Write("Número: ");
+//    numero = int.Parse(Console.ReadLine());
+
+//    if (numero == 0)
+//    {
+//        break;
+//    }
+
+//    if (numero % 2 == 0)
+//    {
+//        somaPares += numero;
+//    }
+//    else
+//    {
+//        somaImpares += numero;
+//    }
+//}
+
+//Console.WriteLine("Soma dos números pares: " + somaPares);
+//Console.WriteLine("Soma dos números ímpares: " + somaImpares);
+//    Console.ReadKey();
+
+//ex8
+        //Console.WriteLine("Digite um número inteiro:");
+        //int numero = int.Parse(Console.ReadLine());
+
+        //int contador = 1;
+        //Console.WriteLine("Números pares de 1 até " + numero + ":");
+
+        
+        //while (contador <= numero)
+        //{
+        //    if (contador % 2 == 0) 
+        //    {
+        //        Console.WriteLine(contador);
+        //    }
+        //    contador++; 
+        //  Console.ReadKey();
+        //}
+
+            
+            
+                
+    
+
+    
